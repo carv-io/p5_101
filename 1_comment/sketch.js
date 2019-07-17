9bafa2f049985f8451d4981c067f96846ef2bacd
Chris Amato <31190871+carv-io@users.noreply.github.com>
2019-07-17 14:32:06 -0400

    Initial commit (upload)

---
+++ 1_comment/sketch.js
@@ -0,0 +1,7 @@
+// A comment!
+
+// I can't write ANYTHING HERE WITH NO CONSEQUENCES!
+
+// but, well, not much happens when I run this program
+
+// G:\My Drive\P5 training\My Code\101\1_comment\index.html
